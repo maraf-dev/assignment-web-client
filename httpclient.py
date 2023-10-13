b01530cd4e46a6ea9a702c7910d7c661f36cdb3d
Marafi Mergani <marafimergani@gmail.com>
2023-10-13 10:48:41 -0600

    handel GET query arguments

--- httpclient.py
+++ httpclient.py
@@ -93,15 +93,12 @@ class HTTPClient(object):
         if port == None:
             port = 80
         self.connect(host, port)
+        if args != None:
+            args = urllib.parse.urlencode(args)
+            path += "?" + args
         request = "GET " + path + " HTTP/1.1\r\n" 
         request += "Host: " + host + "\r\n"
-        request += "User-Agent: mozilla/5.0 (x11; linux x86_64; rv:64.0) gecko/20100101 firefox/64.0\r\n"
-        request += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
-        request += "Accept-Language: en-US,en;q=0.5\r\n"
-        request += "Accept-Encoding: gzip, deflate\r\n"
         request += "Connection: close\r\n"
-        request += "Upgrade-Insecure-Requests: 1\r\n"
-        request += "DNT: 1\r\n"
         request += "\r\n"
         self.sendall(request)
         buffer = self.recvall(self.socket)
